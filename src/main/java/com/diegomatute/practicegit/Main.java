e968c2fba57e497489f8e78ed585e0b9f63d9969
diegomatute <Dmatute43@gmail.com>
2020-09-09 11:49:09 -0400

    first commit

---
+++ src/main/java/com/diegomatute/practicegit/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.diegomatute.practicegit;
+
+/**
+ *
+ * @author diegomatute
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.printf("Hello world\n");
+    }
+}
